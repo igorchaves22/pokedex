193a57aa847895d0f33ac81446c3e25cac905834
Igor Gabriel Vieira Chaves <contato.igorgabriel@hotmail.com>
2024-08-11 18:36:24 -0300

    feat: Absolute imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
@@ -6,5 +7,10 @@ export default defineConfig({
     plugins: [react()],
     server: {
         port: 3000
+    },
+    resolve: {
+        alias: {
+            "~types": resolve(__dirname, "./src/types")
+        }
     }
 });
