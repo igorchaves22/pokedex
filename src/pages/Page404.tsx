1ece0246c4e9da99d6c2b792a86ceff7bdff1d09
Igor Gabriel Vieira Chaves <contato.igorgabriel@hotmail.com>
2024-08-14 17:30:38 -0300

    feat: Page - 404

--- src/pages/Page404.tsx
+++ src/pages/Page404.tsx
@@ -1,5 +1,13 @@
-import { PageLayout } from "~components/molecules";
+import page404Image from "~assets/image/page404.png";
+import { Figure, PageLayout } from "~components/molecules";
 
 export function Page404() {
-    return <PageLayout>Page404</PageLayout>;
+    return (
+        <PageLayout>
+            <Figure
+                text="Page not found!"
+                src={page404Image}
+            />
+        </PageLayout>
+    );
 }
